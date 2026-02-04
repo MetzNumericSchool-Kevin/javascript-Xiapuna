4ca89e4fec6197eaa11f34a224138bb53c6de69d
Célia <celia.godfrin@gmail.com>
2026-02-04 09:52:05 +0100

    04_calcul_prix_total

--- 1_Basics/exercices/04_calcul_prix_total.js
+++ 1_Basics/exercices/04_calcul_prix_total.js
@@ -1,4 +1,7 @@
 // Calcul du prix total d'une commande de potion 🪙
 
 // Variables des exercices précédents (modifie selon tes valeurs)
-let prix_potion_soin = 50;
+let healing_potion_price = 50
+let healing_potion_quantity = parseInt(prompt(`How many healing potion do you want, Adventurer ?`))
+let healing_potion_total_price = console.log(`${healing_potion_quantity} Healing potions : ${healing_potion_price * healing_potion_quantity} 🪙. Adventurer.`)
+
