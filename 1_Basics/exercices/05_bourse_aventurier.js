e1d9e4ac1d7e03de90fe94cbc742b470ea16b80f
Célia <celia.godfrin@gmail.com>
2026-02-04 10:07:11 +0100

    05_bourse_aventurier

--- 1_Basics/exercices/05_bourse_aventurier.js
+++ 1_Basics/exercices/05_bourse_aventurier.js
@@ -1,5 +1,19 @@
 // Bourse de l'Aventurier 💰
 
 // Variables des exercices précédents (modifie selon tes valeurs)
-let stock_potion_soin = 10;
-let prix_potion_soin = 50;
+let healing_potion_stock = 10
+let healing_potion_price = 50
+let purse_money_adventurer = 1500
+let healing_potion_quantity = parseInt(prompt(`How many healing potion do you want, Adventurer ?`))
+let healing_potion_total_price = console.log(`${healing_potion_quantity} Healing potions : ${healing_potion_price * healing_potion_quantity} 🪙. Adventurer.`)
+
+
+
+if ((purse_money_adventurer >= healing_potion_total_price) && (healing_potion_quantity <= healing_potion_stock)) {
+    console.log(`${healing_potion_quantity} Healing potions for you, Adventurer !`)
+    purse_money_adventurer = purse_money_adventurer - healing_potion_total_price
+} else if (purse_money_adventurer < healing_potion_total_price) {
+    console.log(`You don't have enough money, Adventurer !`)
+} else if (healing_potion_quantity > healing_potion_stock) {
+    console.log(`There are only ${healing_potion_stock} in stock, Adventurer !`)
+}
