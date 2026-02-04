1e7125ed6284fa6e6d526674993f84f32a475613
Célia <celia.godfrin@gmail.com>
2026-02-04 09:19:28 +0100

    02_affichage_conditionnel

--- 1_Basics/exercices/02_affichage_conditionnel.js
+++ 1_Basics/exercices/02_affichage_conditionnel.js
@@ -1,5 +1,11 @@
 // Affichage conditionnel
 
 // Variables de l'exercice précédent (modifie selon tes valeurs)
-const nom_boutique = "La Potion Enchantée";
-const boutique_ouverte = true;
+const shop_name = "Leaky Cauldron"
+const leaky_cauldron_open = false
+
+if (leaky_cauldron_open) {
+    console.log(`Welcome to the ${shop_name}'s shop Adventurer !`)
+} else {
+    console.log(`The ${shop_name}'s shop is closed, come back later Adventurer !`)
+}
