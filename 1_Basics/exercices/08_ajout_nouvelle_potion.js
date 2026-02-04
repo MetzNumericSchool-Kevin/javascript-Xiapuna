c658f3bcef1ab77331c40e13defce4a012cb2dcd
Célia <celia.godfrin@gmail.com>
2026-02-04 10:43:43 +0100

    08_ajout_nouvelle_potion

--- 1_Basics/exercices/08_ajout_nouvelle_potion.js
+++ 1_Basics/exercices/08_ajout_nouvelle_potion.js
@@ -1,4 +1,12 @@
 // Ajout d'une nouvelle potion
 
 // Variable de l'exercice précédent (modifie selon tes valeurs)
-const potions = ["Potion de soin", "Potion de mana", "Potion d'endurance"];
+const potions_list = ["Healing potion", "Polyjuice Potion", "Shrinking Potion"]
+
+potions_list.push("Felix Felicis")
+
+console.log(potions_list)
+
+potions_list.pop("Felix Felicis")
+
+console.log(potions_list)
