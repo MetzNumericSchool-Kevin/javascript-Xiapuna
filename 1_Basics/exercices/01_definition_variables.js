c0daec0083af2a1e430e2dfdf8cb19585d2759f3
Célia <celia.godfrin@gmail.com>
2026-02-04 09:10:29 +0100

    01_definition_variables

--- 1_Basics/exercices/01_definition_variables.js
+++ 1_Basics/exercices/01_definition_variables.js
@@ -1,1 +1,7 @@
 // Définition de variables
+const shop_name = "Leaky Cauldron"
+let healing_potion_stock = 10
+let healing_potion_price = 2
+let leaky_cauldron_open = "open"
+
+console.log(`${shop_name} is ${leaky_cauldron_open}. The Healing Potions stock is ${healing_potion_stock}, the price is ${healing_potion_price} par bottle.`)
