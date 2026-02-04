2b0a00151e830305ab21149b9d44c86b512a0971
Célia <celia.godfrin@gmail.com>
2026-02-04 10:49:24 +0100

    10_objet_potion_soin

--- 1_Basics/exercices/10_objet_potion_soin.js
+++ 1_Basics/exercices/10_objet_potion_soin.js
@@ -1,1 +1,11 @@
 // Rangeons les informations de la potion de soin dans un objet 🧹
+let healing_potion_stock = 10
+let healing_potion_price = 50
+
+const potion = {
+    "name" : "Healing potion",
+    "price" : healing_potion_price,
+    "stock" : healing_potion_stock
+}
+
+console.log(potion)
