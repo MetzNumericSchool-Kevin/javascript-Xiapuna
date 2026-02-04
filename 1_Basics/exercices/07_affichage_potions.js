b33aaffe2febd1e95bd8a7a9bd15418e75086503
Célia <celia.godfrin@gmail.com>
2026-02-04 10:39:12 +0100

    07_affichage_potions

--- 1_Basics/exercices/07_affichage_potions.js
+++ 1_Basics/exercices/07_affichage_potions.js
@@ -1,4 +1,11 @@
 // Affichage des potions
 
 // Variable de l'exercice précédent (modifie selon tes valeurs)
-const potions = ["Potion de soin", "Potion de mana", "Potion d'endurance"];
+const potions_list = ["Healing potion", "Polyjuice Potion", "Shrinking Potion"]
+
+console.log(potions_list[0])
+console.log(potions_list[potions_list.length - 1])
+
+for (potion in potions_list) {
+    console.log(`We have ${potions_list[potion]}`)
+}
